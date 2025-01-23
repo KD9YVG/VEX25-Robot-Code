d4866954cb9773f1fd06c140c26391e1d4d22fc2
kd9yvg <brayden.irwin.11@gmail.com>
2025-01-22 18:19:32 -0600

    Moved the files to remove the "more pythonic" from names.

--- twosides.py
+++ twosides.py
@@ -1,23 +1,22 @@
 import os
 
 def do(side):
-    main=open("main more pythonic.py").read()
+    main=open("main.py").read()
     if side=="l":
         main=main.replace('self.VALUE_SIDE="right"','self.VALUE_SIDE="left"')
     else:
         main=main.replace('self.VALUE_SIDE="left"','self.VALUE_SIDE="right"')
-    open("main more pythonic.py","w").write(main)
+    open("main.py","w").write(main)
     print("Left" if side=="l" else "Right","configured.")
-
 
 do("l")
 print("Uploading left/slot 1")
-open(".gitignored/Left.py","w").write(open("main more pythonic.py").read())
+open(".gitignored/Left.py","w").write(open("main.py").read())
 os.system("c:\\Users\\kwate\\.vscode\\extensions\\vexrobotics.vexcode-0.6.1\\resources\\tools\\vexcom\\win32\\vexcom.exe --slot 1 --write .gitignored/Left.py --progress")
 os.remove(".gitignored/Left.py")
 print("\n")
 do("r")
 print("Uploading right/slot 2")
-open(".gitignored/Right.py","w").write(open("main more pythonic.py").read())
+open(".gitignored/Right.py","w").write(open("main.py").read())
 os.system("c:\\Users\\kwate\\.vscode\\extensions\\vexrobotics.vexcode-0.6.1\\resources\\tools\\vexcom\\win32\\vexcom.exe --slot 2 --write .gitignored/Right.py --progress")
 os.remove(".gitignored/Right.py")
