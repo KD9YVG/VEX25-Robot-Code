a2854be47e58962067a0db1cc816244574e6ef7a
kd9yvg <brayden.irwin.11@gmail.com>
2025-03-12 18:57:30 -0500

    Move back and get ready for next move

--- skillsauto.py
+++ skillsauto.py
@@ -67,6 +67,8 @@ class CycloneRobotCodeApp:
         self.move_forward(15,True)
         self.turn_degrees(-60,True)
         self.move_forward(-30,True)
+        self.move_forward(30,True)
+        self.turn_degrees(-90,True)
     def unusedfunction_imtoolazytocomment(self):
         self.Left.set_velocity(50,PERCENT)
         self.turn_degrees(90,True)
